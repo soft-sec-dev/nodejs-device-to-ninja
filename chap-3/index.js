fc0c7e8bc6e3d84d92e064d2cea62829b49865d0
jordanhuaman <jordanelcracken@gmail.com>
2023-09-11 10:38:44 -0500

    feat: before to ask for pull request

---
+++ chap-3/index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+// console.log('hello world')
+// const logs = process.argv
+// logs.forEach(element => {
+//   console.log(element)
+// });
+
+const nameArg = captalize(
+  process.argv[2] || process.env.USER || process.env.USERNAME || "world"
+);
+console.log(nameArg);
+
+function captalize(str) {
+  return str
+    .trim()
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
--- index.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-console.log('hello world')
-const logs = process.argv
-logs.forEach(element => {
-  console.log(element)
-});
