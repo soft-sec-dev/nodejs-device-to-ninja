80052f435456688dc3c8cd188fde9838aca4508e
jordanhuaman <jordanelcracken@gmail.com>
2023-09-16 10:22:26 -0500

    feat: add http.request v 2.0

--- chap-3/server.js
+++ chap-3/server.js
@@ -4,10 +4,25 @@ const port = process.argv[2] || process.env.PORT || 3000,
 http
   .createServer((req, res) => {
     console.log(req.url);
+
+    const nameArg = capitalize(
+      req.url
+        .replace(/[^\w.,-]/g, " ")
+        .replace(/\s+/g, "")
+        .trim() || "world"
+    );
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
-    res.end(`<p>Hello wordlqweqw</p>`);
+    res.end(`<p>Hello ${nameArg}</p>`);
   })
   .listen(port);
 
-console.log(`Server runnin on: http://localhost:${port}`)
+function capitalize(str) {
+  return str
+    .trim()
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+console.log(`Server runnin on: http://localhost:${port}`);
